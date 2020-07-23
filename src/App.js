31fd75375a6a310517839d538cf0a6f3169c52d0
Jude <judelaurentetejada@gmail.com>
2020-07-23 13:45:33 +0800

    Project reboot to redux-saga

--- src/App.js
+++ src/App.js
@@ -1,78 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Movie from "./components/Movie";
-import Person from "./components/Person";
-import Genre from "./components/Genre";
-
-import { MoviesProvider } from "./context/Movies.context";
-
-import ScrollToTop from "./util/scrollTopTop";
-//global style
-import { GlobalStyle, MainWrapper, ContentWrapper } from "./styles/global";
 
 function App() {
-  return (
-    <React.Fragment>
-      <GlobalStyle />
-      <MainWrapper>
-        <Navbar />
-        <ContentWrapper>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => (
-                <MoviesProvider>
-                  <Header />
-                </MoviesProvider>
-              )}
-            />
-            <Route
-              exact
-              path="/movies/:id"
-              render={(routeProps) => {
-                return (
-                  <>
-                    <ScrollToTop>
-                      <Movie {...routeProps} />
-                    </ScrollToTop>
-                  </>
-                );
-              }}
-            />
-
-            <Route
-              exact
-              path="/person/:id"
-              render={(routeProps) => {
-                return (
-                  <>
-                    <ScrollToTop>
-                      <Person {...routeProps} />
-                    </ScrollToTop>
-                  </>
-                );
-              }}
-            />
-
-            <Route
-              exact
-              path="/genres/:id/:genre"
-              render={(routeProps) => {
-                return (
-                  <>
-                    <Genre {...routeProps} />
-                  </>
-                );
-              }}
-            />
-          </Switch>
-        </ContentWrapper>
-      </MainWrapper>
-    </React.Fragment>
-  );
+  return <h1>Movie Project</h1>;
 }
 
 export default App;
--- src/components/Searchbar.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Searchbar() {
-  return <input type="text" name="searchbar" placeholder="Search a Movie" />;
-}
